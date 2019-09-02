a453d0a0575f3405380098faf50711ad150ef971
omkarnagare <omkar.balkrishna@gmail.com>
2019-09-02 18:42:47 +0530

    v1.0: Handling back button press event in home page and settings page, Hiding splash screen only after page loads

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Platform } from '@ionic/angular';
+
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy, AfterViewInit{
 
-  constructor() {}
+  backButtonSubscription$: Subscription;
+
+  constructor(
+    private _platform: Platform
+  ) {}
+
+  ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    SplashScreen.hide();
+  }
+
+  ngAfterViewInit() {
+    this.backButtonSubscription$ = this._platform.backButton.subscribe(() => {
+      navigator['app'].exitApp();
+    });
+  }
+
+  ngOnDestroy() {
+    this.backButtonSubscription$.unsubscribe();
+    this.backButtonSubscription$ = null;
+  }
 
 }
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
   templateUrl: 'settings.page.html',
   styleUrls: ['settings.page.scss']
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage implements OnInit, OnDestroy, AfterViewInit {
+
+  backButtonSubscription$: Subscription;
 
   appVersion: string;
 
-  constructor() {
+  constructor(
+    private _platform: Platform
+  ) {
   }
 
   ionViewDidEnter() {
@@ -17,6 +23,17 @@ export class SettingsPage implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.backButtonSubscription$ = this._platform.backButton.subscribe(() => {
+      navigator['app'].exitApp();
+    });
+  }
+
+  ngOnDestroy() {
+    this.backButtonSubscription$.unsubscribe();
+    this.backButtonSubscription$ = null;
   }
   // add back when alpha.4 is out
   // navigate(item) {
