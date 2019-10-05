890c31ab569d359715cbf34797dc495f95700399
omkarnagare <omkar.balkrishna@gmail.com>
2019-10-05 17:46:26 +0530

    v1.0: Updating privacy policy and terms and conditions from dropbox, Fixing issue with log-out button, Fixing issue with background image on nexus 10 device, Adding screen shots of the app, Adding store listing, Adding bat file for setting up the enironments

---
+++ set_up_environment.bat
@@ -0,0 +1,7 @@
+SET ANDROID_SDK_ROOT=E:\Softwares\AndroidStudio\Android-SDK\sdk
+SET ANDROID_HOME=E:\Softwares\AndroidStudio\Android-SDK\sdk
+SET OPENSSL_HOME=C:\"Program Files"\OpenSSL
+SET CURL_HOME=C:\"Program Files"\curl-7.66.0-win64-mingw
+SET PYTHON=C:\Python37
+SET GOOGLE_APPLICATION_CREDENTIALS=borrowed-8ca5e08456c0.json
+SET PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%ANDROID_HOME%\build-tools\28.0.3;%OPENSSL_HOME%\bin;%CURL_HOME%\bin;%PYTHON%;%PYTHON%\Scripts;
